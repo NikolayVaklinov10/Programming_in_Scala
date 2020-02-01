a9e0727be1fcbe404e87a372593b20d2c85edbef
Nick <nikolayvaklinov@yahoo.com>
2020-02-01 23:55:13 +0000

    additions

--- src/main/scala/chapter4/ChecksumAccumulator.scala
+++ src/main/scala/chapter4/ChecksumAccumulator.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable
+
 
 class ChecksumAccumulator{
   private var sum = 0
@@ -6,7 +8,22 @@ class ChecksumAccumulator{
 }
 
 
+object ChecksumAccumulator{
 
+  private val cache = mutable.Map.empty[String, Int]
+
+  def calculate(s: String): Int =
+    if (cache.contains(s))
+      cache(s)
+  else{
+      val acc = new ChecksumAccumulator
+      for (c <- s)
+        acc.add(c.toByte)
+      val cs = acc.checksum()
+      cache += (s -> cs)
+      cs
+    }
+}
 
 
 
