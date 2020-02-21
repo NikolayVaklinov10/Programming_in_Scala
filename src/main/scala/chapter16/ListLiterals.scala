5a40d6272b19593439eacd89b69ac393312ee86d
Nick <nikolayvaklinov@yahoo.com>
2020-02-21 10:13:49 +0000

    list literals commit

---
+++ src/main/scala/chapter16/ListLiterals.scala
@@ -0,0 +1,17 @@
+package chapter16
+
+
+
+object ListLiterals extends App{
+
+  val fruit = List("apple", "oranges", "pears")
+  val nums = List(1,2,3,4)
+  val diag3 =
+    List(
+      List(1,0,0),
+      List(0,1,0),
+      List(0,0,1)
+    )
+  val empty = List()
+
+}
