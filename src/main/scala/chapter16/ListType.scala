001e3c4a1402572d2972570499cde0a992a82130
Nick <nikolayvaklinov@yahoo.com>
2020-02-21 10:22:12 +0000

    list type commit

---
+++ src/main/scala/chapter16/ListType.scala
@@ -0,0 +1,15 @@
+package chapter16
+
+object ListType extends App {
+
+  val fruit: List[String] = List("apples", "oranges", "pears")
+  val nums: List[Int] = List(1,2,3,4)
+  val diag3: List[List[Int]] =
+    List(
+      List(1,0,0),
+      List(0,1,0),
+      List(0,0,1)
+    )
+  val empty: List[Nothing] = List()
+
+}
